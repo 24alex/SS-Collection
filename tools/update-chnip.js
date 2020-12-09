f443177caf1f0b27b455e4bba8c83ed5dc30d2e6
24alex <alex042824@outlook.com>
2020-12-10 03:36:43 +0800

    Update update-chnip.js

--- tools/update-chnip.js
+++ tools/update-chnip.js
@@ -4,7 +4,7 @@ const { writeFileSync } = require('fs');
 
 const CHNIP_FILE = '../SSR/chn_ip.txt';
 
-get('https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt').then(({ data }) => {
+get('https://raw.githubusercontent.com/Hackl0us/GeoIP-CN/master/artifacts/geoip-cn.txt').then(({ data }) => {
     const CIDRs = data.split(/\s/);
     const ranges = CIDRs.map(CIDR => {
         const { base, broadcast, first, last } = new Netmask(CIDR);
